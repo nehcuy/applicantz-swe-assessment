262cc2319181e556d930388d811830810d52155c
yuchen <hyuchen2607@gmail.com>
2026-01-15 14:20:27 +0800

    Add JUnit tests for Assignment 1

---
+++ assignment1/main/Assignment1Test.java
@@ -0,0 +1,63 @@
+package assignment1.main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test cases for Assignment 1 reverseWords() method.
+ * @author Huang Yuchen
+ * @author hyuchen2607@gmail.com
+ */
+public class Assignment1Test {
+    /**
+     * Test case provided from Problem Statement
+     */
+    @Test
+    public void inputTestEg() {
+        String expected = "gnirtS; eb2 desrever...";
+        String toBeReversed = "String; 2be reversed...";
+        String actual = Assignment1.reverseWords(toBeReversed);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test case with all characters
+     */
+    @Test
+    public void inputTestAllChars() {
+        String expected = "desreverebotsdrowllA";
+        String toBeReversed = "Allwordstobereversed";
+        String actual = Assignment1.reverseWords(toBeReversed);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test case with all numbers
+     */
+    @Test
+    public void inputTestAllNums() {
+        String expected = "0987654321";
+        String toBeReversed = "1234567890";
+        String actual = Assignment1.reverseWords(toBeReversed);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test case with all special characters
+     */
+    @Test
+    public void inputTestAllSpecialChars() {
+        String expected = "!@#$%^ &*()_+.,";
+        String toBeReversed = "!@#$%^ &*()_+.,";
+        String actual = Assignment1.reverseWords(toBeReversed);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void inputTestCharAndNum() {
+        String expected = "321cba";
+        String toBeReversed = "abc123";
+        String actual = Assignment1.reverseWords(toBeReversed);
+        assertEquals(expected, actual);
+    }
+}
